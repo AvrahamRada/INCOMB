89b7886a66ad045b183763d37cd3f0d9b880c154
liortra <liortra@gmail.com>
2020-01-25 15:41:40 +0200

    Adding characters

---
+++ app/src/main/java/android_project/incomb/Fest.java
@@ -0,0 +1,12 @@
+package android_project.incomb;
+
+public class Fest extends Person {
+    // attributes
+    private String type;
+
+    //constructor
+    public Fest(String firstName, String lastName, String email) {
+        super(firstName, lastName, email);
+    }
+
+}
---
+++ app/src/main/java/android_project/incomb/Person.java
@@ -0,0 +1,40 @@
+package android_project.incomb;
+
+abstract class Person {
+    // attributes
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    //constructor
+    Person(String firstName, String lastName, String email) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+    }
+
+    //Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    private void setEmail(String email) {
+        this.email = email;
+    }
+}
