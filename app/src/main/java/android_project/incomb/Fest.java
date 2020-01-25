b595b370abd314680f540b44ace9c2272595222a
liortra <liortra@gmail.com>
2020-01-25 18:26:25 +0200

    Update characters

--- app/src/main/java/android_project/incomb/Fest.java
+++ app/src/main/java/android_project/incomb/Fest.java
@@ -1,12 +1,68 @@
 package android_project.incomb;
 
+import java.io.IOException;
+import java.util.Calendar;
+
 public class Fest extends Person {
     // attributes
-    private String type;
+    private String typeActivity; // yoga, lecture or Social Action
+    private String space;// bar or class or studio
+    private String location; // Physical location
+    private int amountOfGuest;
+    private Calendar dateOfTheActivity; //check how to save dates
 
     //constructor
-    public Fest(String firstName, String lastName, String email) {
+    public Fest(String firstName, String lastName, String email, String typeActivity, String space, String location, int amountOfGuest, Calendar dateOfTheActivity) throws IOException {
         super(firstName, lastName, email);
+        setTypeActivity(typeActivity);
+        setSpace(space);
+        setLocation(location);
+        setAmountOfGuest(amountOfGuest);
+        setDateOfTheActivity(dateOfTheActivity);
     }
 
+    //Getters and Setters
+    public String getTypeActivity() {
+        return typeActivity;
+    }
+
+    private void setTypeActivity(String typeActivity) {
+        this.typeActivity = typeActivity;
+    }
+
+    public String getSpace() {
+        return space;
+    }
+
+    private void setSpace(String space) {
+        this.space = space;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    private void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getAmountOfGuest() {
+        return amountOfGuest;
+    }
+
+    private void setAmountOfGuest(int amountOfGuest) throws IOException {
+        if(amountOfGuest > 0)
+            this.amountOfGuest = amountOfGuest;
+        else {
+            throw new IOException();
+        }
+    }
+
+    public Calendar getDateOfTheActivity() {
+        return dateOfTheActivity;
+    }
+
+    private void setDateOfTheActivity(Calendar dateOfTheActivity) {
+        this.dateOfTheActivity = dateOfTheActivity;
+    }
 }
--- app/src/main/java/android_project/incomb/Guest.java
+++ app/src/main/java/android_project/incomb/Guest.java
@@ -1,12 +1,43 @@
 package android_project.incomb;
 
+import java.util.Calendar;
+
 public class Guest extends Person {
     // attributes
-    private String Location;
+    private String typeActivity; // yoga, lecture or Social Action
+    private String location; // Physical location
+    private Calendar dateOfTheActivity; //check how to save dates
 
     //constructor
-    public Guest(String firstName, String lastName, String email) {
+    public Guest(String firstName, String lastName, String email, String typeActivity, String location, Calendar dateOfTheActivity) {
         super(firstName, lastName, email);
+        setTypeActivity(typeActivity);
+        setLocation(location);
+        setDateOfTheActivity(dateOfTheActivity);
     }
 
+    //Getters and Setters
+    public String getTypeActivity() {
+        return typeActivity;
+    }
+
+    private void setTypeActivity(String typeActivity) {
+        this.typeActivity = typeActivity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    private void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Calendar getDateOfTheActivity() {
+        return dateOfTheActivity;
+    }
+
+    private void setDateOfTheActivity(Calendar dateOfTheActivity) {
+        this.dateOfTheActivity = dateOfTheActivity;
+    }
 }
