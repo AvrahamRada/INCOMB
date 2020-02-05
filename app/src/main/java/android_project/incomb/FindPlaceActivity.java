d8954b364c0546b89ee0f8e3eb779d3d47d7a9fd
StylerGit <stylergit@gmail.com>
2020-02-05 22:07:08 +0200

    FestActivity

---
+++ app/src/main/java/android_project/incomb/FindPlaceActivity.java
@@ -0,0 +1,15 @@
+package android_project.incomb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//אthis is page 9 in incomb app pdf
+public class FindPlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_place);
+    }
+}
