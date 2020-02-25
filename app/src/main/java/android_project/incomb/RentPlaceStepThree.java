6f5aae36fb725cf4583f39ba378034fff899770f
liortra <liortra@gmail.com>
2020-02-25 15:48:32 +0200

    Adding RentPlace steps

---
+++ app/src/main/java/android_project/incomb/RentPlaceStepThree.java
@@ -0,0 +1,14 @@
+package android_project.incomb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RentPlaceStepThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_place_step_three);
+    }
+}
