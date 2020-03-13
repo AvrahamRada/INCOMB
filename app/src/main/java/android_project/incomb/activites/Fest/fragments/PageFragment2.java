025f92832a5bf8dd703376be7079fc69ead11b97
AvrahamRada <avraham1992rada@gmail.com>
2020-03-13 15:03:20 +0200

    adding Map Activitys

---
+++ app/src/main/java/android_project/incomb/activites/Fest/fragments/PageFragment2.java
@@ -0,0 +1,24 @@
+package android_project.incomb.activites.Fest.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android_project.incomb.R;
+
+public class PageFragment2 extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.paper2,container,false);
+
+        return rootView;
+    }
+}
