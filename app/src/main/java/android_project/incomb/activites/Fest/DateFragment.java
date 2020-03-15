766bf56133bca393638e5027896979069aafd885
AvrahamRada <avraham1992rada@gmail.com>
2020-03-16 01:42:12 +0200

    Finish design of FEST

---
+++ app/src/main/java/android_project/incomb/activites/Fest/DateFragment.java
@@ -0,0 +1,29 @@
+package android_project.incomb.activites.Fest;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android_project.incomb.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DateFragment extends Fragment {
+
+    public DateFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_date, container, false);
+    }
+}
--- app/src/main/java/android_project/incomb/activites/Fest/DateRangeActivity.java
+++ app/src/main/java/android_project/incomb/activites/Fest/DateRangeActivity.java
@@ -31,7 +31,7 @@ public class DateRangeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_range);
         datePicker = findViewById(R.id.date);
-        btnSelect = findViewById(R.id.btn);
+        btnSelect = findViewById(R.id.btn_check);
         Slidr.attach(this);
         //Avraham - use this calendar
         initCalender();
@@ -40,44 +40,6 @@ public class DateRangeActivity extends AppCompatActivity {
             Toast.makeText(DateRangeActivity.this, "Days!!", Toast.LENGTH_SHORT).show();
             sendDateSelected();
         });
-    /*
-        final Calendar nextYear = Calendar.getInstance();
-        final Calendar lastYear = Calendar.getInstance();
-
-        nextYear.add(Calendar.YEAR, 10);
-        lastYear.add(Calendar.YEAR, -10);
-
-        calender = findViewById(R.id.date);
-        b = findViewById(R.id.btn);
-
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        calender.deactivateDates(list);
-        ArrayList<java.util.Date> arrayList = new ArrayList<>();
-        try {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-mm-yyyy");
-            String strDate1 = "22-2-2020";
-            String strDate2 = "26-2-2020";
-
-            java.util.Date newdate1 = dateFormat.parse(strDate1);
-            java.util.Date newdate2 = dateFormat.parse(strDate2);
-            arrayList.add(newdate1);
-            arrayList.add(newdate2);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-
-        calender.init(lastYear.getTime(), nextYear.getTime(),
-                new SimpleDateFormat("dd-mm-yyyy", Locale.getDefault()))
-                .inMode(CalendarPickerView.SelectionMode.RANGE)
-                .withSelectedDate(new Date())
-                .withDeactivateDates(list)
-                .withHighlightedDates(arrayList);
-
-        btnSelect.setOnClickListener(v -> Toast.makeText(DateRangeActivity.this, "dxfdz", Toast.LENGTH_SHORT).show());
-    */
-
     }
 
     private void sendDateSelected() {
